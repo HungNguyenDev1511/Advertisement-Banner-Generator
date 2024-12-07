cfc8a2c6470b7c317752fb509794af0d744be2f9
HungNguyen686868 <nguyenphunghung1511@gmail.com>
2024-12-07 07:48:59 +0700

    instruction

---
+++ InstructPix2Pix/stable_diffusion/ldm/modules/image_degradation/__init__.py
@@ -0,0 +1,2 @@
+from InstructPix2Pix.stable_diffusion.ldm.modules.image_degradation.bsrgan import degradation_bsrgan_variant as degradation_fn_bsr
+from InstructPix2Pix.stable_diffusion.ldm.modules.image_degradation.bsrgan_light import degradation_bsrgan_variant as degradation_fn_bsr_light
---
+++ InstructPix2Pix/stable_diffusion/ldm/modules/losses/__init__.py
@@ -0,0 +1,1 @@
+from InstructPix2Pix.stable_diffusion.ldm.modules.losses.contperceptual import LPIPSWithDiscriminator
